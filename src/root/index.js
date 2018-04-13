5d8a8199753b9aa9de55c7020867438270e55d56
Serhii Minin <sergey.minin@brainberry.ua>
2018-04-13 17:11:50 +0300

    fulfill root component

--- src/root/index.js
+++ src/root/index.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { api } from './../helpers';
 
-const Root = () => <div>Root</div>;
+class Root extends Component {
+  state={
+    news: [],
+  };
+  componentDidMount() {
+    api({ country: 'ua', category: 'sport', pageSize: 100, q: ''})
+      .then(response => this.setState({news: response.articles}))
+  }
+  render() {
+    return (
+      <div className="Root">
+        {this.state.news.map(news => (
+          <div key={news.title}>
+            <img src={news.urlToImage} alt={news.url} />
+            <p>{news.title}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Root;
