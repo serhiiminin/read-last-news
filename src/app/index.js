9e8b54d700736d399996ba0f9d8045a03e0ba6ee
Serhii Minin <sergey.minin@brainberry.ua>
2018-05-22 15:22:41 +0300

    add notifications for sources

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import injectSheet from 'react-jss';
 import PropTypes from 'prop-types';
 import { compose } from 'recompose';
-import { Main, Header, Sidebar, Footer } from './../components';
+import { Main, Header, Sidebar, Footer, ErrorBoundary } from './../components';
 import styles from './styles';
 
 type Props = {
@@ -14,14 +14,16 @@ const App = ({ classes }: Props) => (
   <div className={classes.container}>
     <Header />
     <div className={classes.content}>
-      <div className={classes.content__sidebar}>
-        <Sidebar />
-      </div>
-      <div className={classes.content__main}>
-        <Main />
-      </div>
-      <Footer />
+      <ErrorBoundary>
+        <div className={classes.content__sidebar}>
+          <Sidebar />
+        </div>
+        <div className={classes.content__main}>
+          <Main />
+        </div>
+      </ErrorBoundary>
     </div>
+    <Footer />
   </div>
 );
 
